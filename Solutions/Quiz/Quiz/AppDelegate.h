6b86f7f1283ed2964a53886314ebd8900b415fc7
梁世平 <shiping@MacBook-Pro.local>
2016-03-09 00:31:17 +0800

    添加项目

---
+++ Solutions/Quiz/Quiz/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by 梁世平 on 16/3/5.
+//  Copyright © 2016年 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ Solutions/Quiz/Quiz/BNRQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRQuizViewController.h
+//  Quiz
+//
+//  Created by 梁世平 on 16/3/5.
+//  Copyright © 2016年 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRQuizViewController : UIViewController
+
+@end
